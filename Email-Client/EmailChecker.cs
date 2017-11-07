821cf87942a0fedce95416024c6aeb017d127c83
Vikram Pal <vikrampal659@gmail.com>
2017-11-08 01:13:09 +0530

    Change show last message to show unread messages

--- Email-Client/EmailChecker.cs
+++ Email-Client/EmailChecker.cs
@@ -10,9 +10,8 @@ namespace Email_Client
 {
     class EmailChecker
     {
-        public static List<Message> FetchNMessages(int nMessagesToFetch, string hostname, int port, bool useSsl, string username, string password)
+        public static List<Message> getUnreadMessages(string hostname, int port, bool useSsl, string username, string password)
         {
-            // The client disconnects from the server when being disposed
             using (Pop3Client client = new Pop3Client())
             {
                 // Connect to the server
@@ -25,19 +24,19 @@ namespace Email_Client
                 int messageCount = client.GetMessageCount();
 
                 // We want to download all messages
-                List<Message> messages = new List<Message>(nMessagesToFetch);
+                List<Message> allMessages = new List<Message>(messageCount);
 
                 // Messages are numbered in the interval: [1, messageCount]
                 // Ergo: message numbers are 1-based.
                 // Most servers give the latest message the highest number
-                for (int i = 0; i < nMessagesToFetch; i++)
+                for (int i = messageCount; i > 0; i--)
                 {
-                    messages.Add(client.GetMessage(messageCount - i));
+                    allMessages.Add(client.GetMessage(i));
                 }
 
                 // Now return the fetched messages
-                return messages;
+                return allMessages;
             }
-        }
+        } 
     }
 }
